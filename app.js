06ad29a2cc57ae58db91b9d76f82827dcb43118d
root <root@LAPTOP-OKSV9T3R.localdomain>
2019-09-05 14:14:23 +0900

    create web project

---
+++ app.js
@@ -0,0 +1,26 @@
+import * as mysqlConfig from './config/mysql';
+const express = require('express');
+var cors = require('cors');
+const app = express();
+var mysql = require('mysql');
+var connection = mysql.createConnection(mysqlConfig.mysqlConfig);
+const port = 3003;
+
+connection.connect();
+
+connection.query('select * from test', function(err, rows, fields) {
+    if (!err) {
+        console.log('Success ', rows)
+    } else {
+        console.log('err ',err)
+    }
+})
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static('public'));
+connection.end();
+
+app.listen(port, () => {
+    console.log("Server "+port+"is start....");
+})
